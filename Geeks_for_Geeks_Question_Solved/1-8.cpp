75b7195826c9b9db6a22f0906c85e154e7236110
Abhijeet Gawas <abhijeetgawas19@gmail.com>
2021-05-05 18:34:25 +0530

    Create 1-8.cpp

---
+++ Geeks_for_Geeks_Question_Solved/1-8.cpp
@@ -0,0 +1,13 @@
+void twoDimensional(int **matrix,int N){
+    
+    // Loop to iterate through matrix
+    for(int i = 0;i<N;i++){
+        for(int j = 0;j<N;j++){
+            
+            cout<<matrix[i][j]<<" ";
+            
+        }
+        cout << endl;
+    }
+    
+}
