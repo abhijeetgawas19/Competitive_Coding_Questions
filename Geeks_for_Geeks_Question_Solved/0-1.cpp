d95966e470fc7ad9f047cd782b4e3d0ea266a85c
Abhijeet Gawas <abhijeetgawas19@gmail.com>
2021-05-17 19:06:15 +0530

    Create 0-1.cpp

---
+++ Geeks_for_Geeks_Question_Solved/0-1.cpp
@@ -0,0 +1,15 @@
+class Solution{
+    public:
+        int absolute(int I) {
+        // Your code goes here
+        if(I>0)
+        {
+            return I;
+        }
+        else
+        {
+            return I * (-1);
+        }
+    }
+
+};
